97c513c5973b6a5d04983a10cb5327978390e684
Wise Ilya <johndoe@example.com>
2019-04-16 18:16:28 +0300

    more chane

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+  function MyApp(props) {
+    return(
+      <div className='AppStyle'>
+        <div className='positionBox'>
+          <input type='checkbox'/>
+          <p>Text here</p>
+        </div>
 
-export default App;
+        <div className='positionBox'>
+          <input type='checkbox'/>
+          <p>Text here</p>
+        </div>
+
+        <div className='positionBox'>
+          <input type='checkbox'/>
+          <p>Text here</p>
+        </div>
+
+        <div className='positionBox'>
+          <input type='checkbox'/>
+          <p>Text here</p>
+        </div>
+
+      </div>
+    )
+  }
+
+
+  const style = {
+    fontWeight: 'bold',
+    fontSize: 20px,
+    display: 'flex'
+  }
+
+
+  export default MyApp
